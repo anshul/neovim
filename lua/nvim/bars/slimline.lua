1a0abe8f850a2375e5053cabcbb12192bb2cfe52
Anshul Khandelwal <anshul@anshul.io>
2025-06-30 15:50:57 +0530

    fix: remove redundant slimline highlight creation call
    The slimline.highlights.create_hls() call was redundant since custom highlights
    are already being created manually with the Catppuccin palette.

--- lua/nvim/bars/slimline.lua
+++ lua/nvim/bars/slimline.lua
@@ -66,7 +66,6 @@ require('lze').load {
           },
         },
       }
-      require('slimline.highlights').create_hls()
       local colors = require('catppuccin.palettes').get_palette 'mocha'
       local set_hl_primary = function(name, fg_color, bg_color)
         vim.api.nvim_set_hl(0, name, { fg = fg_color, bg = bg_color, italic = true, bold = true })
